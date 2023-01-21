52050111229be54b2ae253328207d54cc1425c47
wangkodok <wangkodok@gmail.com>
2023-01-21 22:50:55 +0900

    리액트 컴포넌트, CSS 스타일 모듈

--- movie/src/App.js
+++ movie/src/App.js
@@ -1,7 +1,11 @@
+import Button from "./Button";
+import styled from './App.module.css';
+
 function App() {
   return (
     <div>
-      <h1>Welcome back!!!</h1>
+      <h1 className={styled.title}>Welcome back!!!</h1>
+      <Button text={"Continue"} />
     </div>
   );
 }
