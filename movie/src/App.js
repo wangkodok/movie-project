6d2e638ab4c7c96a4f14b57f37ca469c105773a2
wangkodok <wangkodok@gmail.com>
2023-01-22 15:28:25 +0900

    리액트 useEffect

--- movie/src/App.js
+++ movie/src/App.js
@@ -1,16 +1,32 @@
 import Button from "./Button";
 import styled from './App.module.css';
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => {
     return prev + 1
   });
-  console.log('call an api');
+  const onChange = (event) => {
+    setKeyword(event.target.value);
+  }
+  useEffect(() => {
+    console.log('call the api');
+  }, []);
+  useEffect(() => {
+    console.log('counter가 변경되면 실행');
+  }, [counter]);
+  useEffect(() => {
+    console.log('keyword가 변경되면 실행');
+  }, [keyword]);
+  useEffect(() => {
+    console.log('counter, keyword 둘 중 하나가 변경되면 실행');
+  }, [counter, keyword]);
 
   return (
     <div>
+      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
       <h1>{counter}</h1>
       <button onClick={onClick}>click me</button>
     </div>
