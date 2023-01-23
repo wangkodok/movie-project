8ba07c2a0c8cd5851f07cb54aa8dac3624156845
wangkodok <wangkodok@gmail.com>
2023-01-23 18:27:59 +0900

    리액트 todo list

--- movie/src/App.js
+++ movie/src/App.js
@@ -1,37 +1,41 @@
-import Button from "./Button";
-import styled from './App.module.css';
-import { useState, useEffect } from "react";
-
-function Hello() {
-  useEffect(function() {
-    console.log('hi :)');
-    return function() {
-      console.log('bye :(');
-    }
-  }, []);
-  useEffect(() => {
-    console.log('hi :)');
-    return () => {
-      console.log('bye :(');
-    }
-  }, []);
-  return (
-    <h1>Hello</h1>
-  )
-}
+import { useState } from "react";
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => {
-    setShowing((prev) => {
-      return !prev;
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => {
+    setToDo(event.target.value);
+  }
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos(function(currentArray) {
+      return (
+        [toDo, ...currentArray]
+      )
     })
+    setToDo("");
   }
 
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My To Dos: {toDos.length}</h1>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={toDo} test="text" placeholder="Write your to do..." />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {
+          toDos.map((value, index) => {
+            return (
+              <li key={index}>{value}</li>
+            )
+          })
+        }
+      </ul>
     </div>
   );
 }
